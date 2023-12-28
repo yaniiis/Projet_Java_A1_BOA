8cc6005f4ea62f30d3b4272fa3338868b81dcc56
Yanis BOUCHAIB <yanisbouchaib28@gmail.com>
2023-12-28 18:16:42 +0100

    Creation de la page de profil

--- src/main/java/com/example/wallet_boa/modele/Investor.java
+++ src/main/java/com/example/wallet_boa/modele/Investor.java
@@ -6,29 +6,48 @@ public class Investor {
     private String surname;
     private String email;
     private String phone_number;
+    private int id;
 
-    public Investor(String name,String surname,String email,String phone_number){
+    public Investor(String name,String surname,String email,String phone_number, int id){
         this.surname = surname;
         this.email = email;
         this.phone_number = phone_number;
         this.name = name;
+        this.id = id;
     }
 
     public String getName(){
         return name;
     }
+    public void setName(String name){
+        this.name = name;
+    }
 
+    public int getId(){
+        return this.id;
+    }
 
     public String getPhone_number(){
-        return phone_number;
+        return this.phone_number;
+    }
+    public void setPhone_number(String Phone_number){
+        this.phone_number = Phone_number;
     }
 
     public String getEmail(){
         return email;
     }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+
 
     public String getSurname(){
         return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
     }
 
 }
